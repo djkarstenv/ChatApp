69e572048d62add5bbcf335b30cd2372c34653d2
Karsten Vermeulen <djkarstenv@gmail.com>
2020-10-21 19:08:00 +0100

    Added code to connect to website

--- Chat_App/Main.cpp
+++ Chat_App/Main.cpp
@@ -24,13 +24,64 @@ int main(int argc, char* argv[])
 	std::cout << "| Welcome to Chat Master 2000 |" << std::endl;
 	std::cout << "#=============================#" << std::endl;
 
-	std::cout << "Waiting for a chat";
+	IPaddress ip;
 
-	while (1)
+	if (SDLNet_ResolveHost(&ip, "www.londonmet.ac.uk", 80) == -1)
 	{
-		std::cout << ".";
-		SDL_Delay(500);
+		std::cout << "Error establishing connection with website." << std::endl;
+		system("pause");
+		return 0;
 	}
+
+	TCPsocket socket = SDLNet_TCP_Open(&ip);
+
+	if (!socket)
+	{
+		std::cout << "Error opening up socket for connection." << std::endl;
+		system("pause");
+		return 0;
+	}
+
+	else
+	{
+		//special request to get HTML code from website
+		std::string message = "GET /HTTPS/1.0\r\n\r\n";
+
+		//we need length of message in order to send data
+		//we add an extra space for the terminating null '\0'
+		int messageLength = message.length() + 1;
+
+		//send message via open socket that we opened up above
+		//if return value is less than length of message then error occurred
+		if (SDLNet_TCP_Send(socket, message.c_str(), messageLength) < messageLength)
+		{
+			std::cout << "Error sending request to website." << std::endl;
+		}
+
+		else
+		{
+			const int BUFFER_SIZE = 2000;
+
+			char response[BUFFER_SIZE];
+
+			if (SDLNet_TCP_Recv(socket, response, BUFFER_SIZE) <= 0)
+			{
+				std::cout << "Error receiving message from website." << std::endl;
+			}
+
+			else
+			{
+				std::cout << response << std::endl;
+			}
+		}
+
+		SDLNet_TCP_Close(socket);
+
+	}
+
+	//shutdown tasks
+	SDLNet_Quit();
+	SDL_Quit();
 
 	system("pause");
 	return 0;
