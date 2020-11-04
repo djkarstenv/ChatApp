65384e4295d3bddeed22071d0699a79d65996c0f
Karsten Vermeulen <djkarstenv@gmail.com>
2020-11-04 19:41:50 +0000

    Added a 'TCPConnection' class on the SERVER side

---
+++ Chat_App/TCPConnection.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+#include <SDL.h>
+#include <SDL_net.h>
+
+const int BUFFER_SIZE = 2000;
+
+class TCPConnection
+{
+public:
+
+	TCPConnection();
+
+public:
+
+	bool Initialize(const char* address, int port);
+	bool OpenSocket();
+	bool ListenSocket();
+	
+	void SendMessage(const std::string& message);
+	void ReceiveMessage(std::string& message);
+
+	void CloseSocket();
+	void Shutdown();
+
+private:
+
+	IPaddress m_ip;
+	TCPsocket m_listenSocket;
+	TCPsocket m_clientSocket;
+
+};
